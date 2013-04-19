380e7636c14532b1da5a40485255ace908252d2b
plong <paul.long@danparis.fr>
2013-04-19 16:38:10 +0200

    add animation

---
+++ js/directives/AddMasonryImageDirective.ts
@@ -0,0 +1,34 @@
+/// <reference path='../_all.ts' />
+module wall {
+    'use strict';
+
+    export class AddMasonryImageDirective {
+
+        public restrict : string;
+        public transclude : bool;
+        public link: ($scope : ng.IScope, element : JQuery, attributes : any) => any;
+
+        public injection(): any[]  {
+            return [
+                () => { return new AddMasonryImageDirective(); }
+            ]
+        }
+
+        constructor(
+            ) {
+            this.restrict = 'A';
+            this.link = ($scope, element, attributes) => this.linkFn($scope, element, attributes);
+        }
+
+        linkFn($scope : ng.IScope, element : JQuery, attributes : any) : any {
+            console.log("AddMasonryImageDirective " + $scope);
+            setTimeout(function(val) { //looks like timeout or deferred solves the issue
+                $('.masonry').imagesLoaded(function() {
+                    $('.masonry').masonry('reload');
+                    element.parent().parent().css({opacity : 1});
+                });
+            }, 1000);
+        }
+
+    }
+}
